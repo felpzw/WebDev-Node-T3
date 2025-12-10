77c8c56d5d703e1f59c29d20cb1270d4b04c34b4
felpzw <lipewinsch@gmail.com>
2025-12-10 10:10:58 -0300

    icon

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -13,13 +13,12 @@ self.addEventListener("push", function (event) {
     event.waitUntil(
         self.registration.showNotification(data.title || "Nova Notícia", {
             body: data.body || "Confira as novidades!",
-            icon: "/img/icons/icon-192x192.png", // certifique-se que o ícone existe
+            icon: "/img/icons/icon-192x192.png", 
             data: { url: data.url || "/" } // Guarda a URL para usar no clique
         })
     );
 });
 
-// Evento disparado quando o usuário clica na notificação
 self.addEventListener("notificationclick", function (event) {
     event.notification.close();
     event.waitUntil(
